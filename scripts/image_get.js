cb5955ed1d32f877257054978756b5c638bc9a40
Lughaigh-w <fyf105@hotmail.com>
2024-04-07 22:16:53 +0100

    removed env

--- scripts/image_get.js
+++ scripts/image_get.js
@@ -1,38 +1,14 @@
 // libraries
 const axios = require('axios');
 const fs = require('fs');
-
-
-// env
 require('dotenv').config();
-const apiKey = process.env.apiKey;
-
-console.log(apiKey)
 
 
-zen_url = "https://zenquotes.io/api/"
-
+// image_url = process.env.image_url
 image_url = "https://zenquotes.io/api/image"
 
-// returns 50 quotes
-quotes = "https://zenquotes.io/api/quotes/"
 
-// today's quote
-today = "https://zenquotes.io/api/today/"
-
-// random quote
-random = "https://zenquotes.io/api/random/"
-
-//axios.get(random)
- // .then(response => {
-//    // console.log(response.status);
-//    console.log('Response data:', response.data);
-//  })
-//  .catch(error => {
-//    console.error('Error:', error);
-//  });
-
-// saves image to file
+// gets an image from zenquotes and saves it
 axios({
   method: 'get',
   url: image_url,
@@ -41,12 +17,10 @@ axios({
 .then(response => {
   fs.writeFileSync('assets/dailyimage.jpg', response.data);
 
-  const image_res = response.data;
+  // nothing is done with the image atm
+  // const image_res = response.data;
 
 })
 .catch(error => {
   console.error('Error:', error);
 });
-
-
-
